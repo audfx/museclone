a0694332a28bff3649b369e6b9b370f6a2a18436
Local Atticus <local.atticus@gmail.com>
2019-12-10 22:13:50 -0500

    Create base layer

---
+++ museclone/MscLayer.cs
@@ -0,0 +1,21 @@
+﻿using theori;
+using theori.Resources;
+
+using MoonSharp.Interpreter;
+
+namespace Museclone
+{
+    public class MscLayer : Layer
+    {
+        public readonly Table tblMsc;
+
+        public MscLayer(ClientResourceLocator locator, string layerPath, DynValue[] args)
+            : base(locator, layerPath, args)
+        {
+            tblMsc = m_script.NewTable();
+        }
+
+        protected override Layer CreateNewLuaLayer(string layerPath, DynValue[] args) =>
+            new MscLayer(ResourceLocator, layerPath, args);
+    }
+}
